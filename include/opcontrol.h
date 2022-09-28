ebe0d839265bac6c695449cc4e98a12fdf55f139
PrinceKomali <taborinstein@gmail.com>
2022-09-28 14:48:15 -0500

    Fix Flywheel [9/28//22]

--- include/opcontrol.h
+++
@@ -1,4 +0,0 @@
-#define X1 controller_get_analog(CONTROLLER_MASTER, ANALOG_LEFT_X)
-#define Y1 controller_get_analog(CONTROLLER_MASTER, ANALOG_LEFT_Y)
-#define X2 controller_get_analog(CONTROLLER_MASTER, ANALOG_RIGHT_X)
-#define Y2 controller_get_analog(CONTROLLER_MASTER, ANALOG_RIGHT_Y)
--- src/opcontrol.c
+++ src/opcontrol.c
@@ -5,10 +5,10 @@
 #include "pros/motors.h"
 #include "pros/screen.h"
 
-#define X1 controller_get_analog(CONTROLLER_MASTER, ANALOG_LEFT_X)
-#define Y1 controller_get_analog(CONTROLLER_MASTER, ANALOG_LEFT_Y)
-#define X2 controller_get_analog(CONTROLLER_MASTER, ANALOG_RIGHT_X)
-#define Y2 controller_get_analog(CONTROLLER_MASTER, ANALOG_RIGHT_Y)
+#define X1 (int)((float)controller_get_analog(CONTROLLER_MASTER, ANALOG_LEFT_X)*0.787*2)
+#define Y1 (int)((float)controller_get_analog(CONTROLLER_MASTER, ANALOG_LEFT_Y)*0.787*2)
+#define X2 (int)((float)controller_get_analog(CONTROLLER_MASTER, ANALOG_RIGHT_X)*0.787*2)
+#define Y2 (int)((float)controller_get_analog(CONTROLLER_MASTER, ANALOG_RIGHT_Y)*0.787*2)
 #define A controller_get_digital(CONTROLLER_MASTER, E_CONTROLLER_DIGITAL_A)
 #define B controller_get_digital(CONTROLLER_MASTER, E_CONTROLLER_DIGITAL_B)
 
@@ -17,7 +17,8 @@
 int a_toggle = 0;
 int pressed_a = 0;
 void opcontrol() {
-	while (1) {
+		while (1) {
+		// controller_set_text(E_CONTROLLER_MASTER, 1, 1, const char *str)
 		if(A && !pressed_a) {
 			pressed_a = 1;
 			a_toggle = !a_toggle;
@@ -32,12 +33,12 @@ void opcontrol() {
 			motor_brake(BottomIntake);
 		}
 
-		motor_move_velocity(Fly1, B * -(Fly1 * FLY_SPEED));
-		motor_move_velocity(Fly2, B * -(Fly1 * FLY_SPEED));
+		motor_move_velocity(Fly1, B * -(600 * FLY_SPEED));
+		motor_move_velocity(Fly2, B * (600 * FLY_SPEED));
 
 		motor_move_velocity(RightFront, (X2 + (X1 - Y1)));
-		motor_move_velocity(LeftFront, (-X2 - (X1 + Y1)));
-		motor_move_velocity(RightBack, ((-X2 + (X1 + Y1)))*-1);
-        motor_move_velocity(LeftBack, ((X2 - (X1 - Y1)))*-1);
+		motor_move_velocity(LeftFront, (-X2 - (X1 + Y1)) );
+		motor_move_velocity(RightBack, ((-X2 + (X1 + Y1))) * -1);
+        motor_move_velocity(LeftBack, ((X2 - (X1 - Y1))) * -1);
 	}
 }
